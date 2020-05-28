9e7f2c5d2bfc012831a31db88effd51545178c31
SebastianJauregui <48133360+SebastianJauregui@users.noreply.github.com>
2020-05-27 21:33:14 -0500

    Update keys.js

--- src/keys.js
+++ src/keys.js
@@ -2,10 +2,10 @@ module.exports = {
 
     database: {
         connectionLimit: 10,
-        host: 'localhost',
-        user: 'megaplaza-dev',
-        password: 'pass123',
-        database: 'pramiv3'
+        host: 'us-cdbr-east-05.cleardb.net',
+        user: 'be948f683b04f4',
+        password: '066773f6',
+        database: 'heroku_039d6b34b02ad26'
     }
 
 };
